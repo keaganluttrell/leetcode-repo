aa8de1ebb41694178405cd7e71313748c3e969b4
keagan <keaganluttrell7@gmail.com>
2024-08-19 07:29:24 -0500

    init

---
+++ lib/two-pointers.c
@@ -0,0 +1,27 @@
+\*
+ * Process for problem solving
+ *
+ * Clarify the problem:
+ *
+ * Define the Problem:
+ *
+ * Constraints:
+ *
+ * Inputs:
+ *
+ * Outputs:
+ *
+ * Edge Cases:
+ *
+ * Come up with a solution in pseudocode
+ *
+ * Solve the problem
+ *
+ * Analyze Performance
+ *
+ * Refactor to Optimal Solution
+*/
+
+
+#include <two-pointers.h>
+
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int charc, char *argv[]) {
+    printf("hello world");
+    return 0;
+}
