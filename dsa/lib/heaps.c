6c6e18d14a3bb1d3022112e9779ca42565f8bb31
keagan <keaganluttrell7@gmail.com>
2024-09-20 17:16:43 -0500

    heaps iii

--- dsa/lib/heaps.c
+++ dsa/lib/heaps.c
@@ -1,23 +1,84 @@
 #include "heaps.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int min_stone_comp(const void *a, const void *b) {
-    return (*(int *)b - *(int *)a);
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-int minStoneSum(int *piles, int pilesSize, int k) {
-    int sum, i, ops;
-    sum = 0, ops = 0;
-    for (i = 0; i < pilesSize; ++i) {
+typedef struct maxHeap {
+    int *h;
+    int len;
+    int Msize;
+} Heap;
+
+void create(Heap *a, int n) {
+    a->h = (int *)malloc(sizeof(int) * n);
+    a->len = 0;
+    a->Msize = n;
+}
+
+int left(int i) { return 2 * i + 1; }
+
+int right(int i) { return 2 * i + 2; }
+
+int parent(int i) { return (i - 1) / 2; }
+
+void maxHeapify(Heap *a, int i) {
+    int large = i, l = left(i), r = right(i);
+    if (l < a->len && a->h[l] > a->h[large]) {
+        large = l;
+    }
+    if (r < a->len && a->h[r] > a->h[large]) {
+        large = r;
+    }
+    if (i != large) {
+        swap(&a->h[i], &a->h[large]);
+        maxHeapify(a, large);
+    }
+}
+
+void push(Heap *a, int x) {
+    int i = a->len++;
+    a->h[i] = x;
+    while (i > 0 && a->h[i] > a->h[parent(i)]) {
+        swap(&a->h[i], &a->h[parent(i)]);
+        i = parent(i);
+    }
+}
+
+int top(Heap *a) { return a->h[0]; }
+
+bool empty(Heap *a) { return a->len == 0; }
+
+void pop(Heap *a) {
+    a->h[0] = a->h[--a->len];
+    maxHeapify(a, 0);
+}
+
+int minStoneSum(int *piles, int n, int k) {
+    Heap *heap = malloc(sizeof(*heap));
+    create(heap, n);
+    int i, v, h, sum;
+
+    sum = 0;
+    for (i = 0; i < n; ++i) {
+        push(heap, piles[i]);
         sum += piles[i];
     }
 
-    qsort(piles, pilesSize, sizeof(int), min_stone_comp);
-    for (i = 0; i < pilesSize; ++i) {
-        printf("%d ", piles[i]);
-    }
-    printf("\n");
+    while (k-- > 0) {
+        v = top(heap);
+        pop(heap);
 
+        h = v / 2;
+        sum -= h;
+        push(heap, v - h);
+    }
+
+    free(heap);
     return sum;
 }
