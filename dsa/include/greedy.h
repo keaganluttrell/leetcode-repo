4e17df69ca3def5b7782fed4c5eced97754f16a5
keagan <keaganluttrell7@gmail.com>
2024-09-25 09:56:15 -0500

    greedy ii

--- dsa/include/greedy.h
+++ dsa/include/greedy.h
@@ -2,5 +2,6 @@
 #define GREEDY_H
 
 int maximum69Number(int num);
+int maximumUnits(int **boxTypes, int n, int *nc, int truckSize);
 
 #endif
--- dsa/lib/greedy.c
+++ dsa/lib/greedy.c
@@ -1,6 +1,7 @@
 #include "greedy.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int maximum69Number(int num) {
     int start, digit, n;
@@ -21,4 +22,27 @@ int maximum69Number(int num) {
     }
 
     return num;
+}
+
+// boxTypes[index] = [numberOfBoxes, numberOfUnitsPerBox]
+// number of boxes can't exceed truck size
+int maximumUnits(int **boxTypes, int n, int *nc, int truckSize) {
+
+    // assume sorted for now
+    int total_boxes = 0, total_units = 0;
+    int i, j, boxes, units;
+
+    for (i = 0; i < n && total_boxes < truckSize; ++i) {
+        boxes = boxTypes[i][0];
+        units = boxTypes[i][1];
+        if ((boxes + total_boxes) < truckSize) {
+            total_boxes += boxes;
+            total_units += (units * boxes);
+        } else {
+            total_units += (units * (truckSize - total_boxes));
+            total_boxes = truckSize;
+        }
+    }
+
+    return units;
 }
