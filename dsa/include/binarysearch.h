8db87a42ef478724fc82afb6ad5d7a2d7e8f6062
keagan <keaganluttrell7@gmail.com>
2024-09-28 18:48:53 -0500

    binary search ii

--- dsa/include/binarysearch.h
+++ dsa/include/binarysearch.h
@@ -1,5 +1,7 @@
 #ifndef BINARYSEARCH_H
 #define BINARYSEARCH_H
 int searchInsert(int *nums, int numsSize, int target);
+int *answerQueries(int *nums, int numsSize, int *queries, int queriesSize,
+                   int *returnSize);
 
 #endif
--- dsa/lib/binarysearch.c
+++ dsa/lib/binarysearch.c
@@ -14,3 +14,39 @@ int searchInsert(int *nums, int n, int target) {
 
     return l;
 }
+
+int comparator(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
+
+int *answerQueries(int *nums, int numsSize, int *queries, int queriesSize,
+                   int *returnSize) {
+    int *result = (int *)malloc(queriesSize * sizeof(int));
+    *returnSize = queriesSize;
+
+    qsort(nums, numsSize, sizeof(int), comparator);
+
+    int *prefixSum = (int *)malloc(numsSize * sizeof(int));
+    prefixSum[0] = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        prefixSum[i] = prefixSum[i - 1] + nums[i];
+    }
+
+    for (int i = 0; i < queriesSize; i++) {
+        int query = queries[i];
+        int left = 0, right = numsSize - 1;
+        int maxSubsequenceSize = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (prefixSum[mid] <= query) {
+                maxSubsequenceSize = mid + 1;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        result[i] = maxSubsequenceSize;
+    }
+
+    free(prefixSum);
+    return result;
+}
